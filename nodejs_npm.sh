9966067ad95880a95b98a35cf0ab1ca33c6aefe2
shiv <shivramkgithub@gmail.com>
2020-08-08 00:38:48 +0530

    Nodejs npm installation shell script

---
+++ nodejs_npm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ ! -x /usr/bin/nodejs ]; then
+echo
+echo
+        echo "INSTALLING nodejs & npm"
+echo
+echo
+        curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+	apt-get install -y nodejs
+	mkdir "${HOME}/.npm-packages"
+	npm config set prefix "${HOME}/.npm-packages"
+	echo 'NPM_PACKAGES="${HOME}/.npm-packages"' >> /etc/bash.bashrc
+	echo 'export PATH="$NPM_PACKAGES/bin:$PATH"' >> /etc/bash.bashrc
+	echo '# Unset manpath so we can inherit from /etc/manpath via the `manpath` command unset MANPATH # delete if you already modified MANPATH elsewhere in your config export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"' >> /etc/bash.bashrc
+echo
+echo
+	echo "SUCESSFULLY INSTALLED nodejs & npm";
+
+
+else
+echo
+echo
+        echo "ALREADY INSTALLED nodejs & npm"
+fi
